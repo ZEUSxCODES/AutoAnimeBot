3e843fcf42b88e89f66b8a19e7c1cb0021053a6a
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-22 06:31:41 +0530

    improve

--- bot/config.py
+++ bot/config.py
@@ -1,35 +1,31 @@
-#    This file is part of the AutoAnime distribution.
-#    Copyright (c) 2023 Kaif_00z
+# This file is part of the AutoAnime distribution.
+# Copyright (c) 2023 Kaif_00z
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
 #
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
 #
-# License can be found in <
-# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
+# License can be found in <https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE> .
 
+import os
 from decouple import config
-
 
 class Var:
     # Telegram Credentials
-
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    API_ID = config("TELEGRAM_API_ID", default=6, cast=int)
+    API_HASH = config("TELEGRAM_API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
     BOT_TOKEN = config("BOT_TOKEN", default=None)
 
     # Database Credentials
-
     REDIS_URI = config("REDIS_URI", default=None)
     REDIS_PASS = config("REDIS_PASSWORD", default=None)
 
     # Channels Ids
-
     BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=0, cast=int)
     MAIN_CHANNEL = config("MAIN_CHANNEL", cast=int)
     LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
@@ -37,10 +33,11 @@ class Var:
     OWNER = config("OWNER", default=0, cast=int)
 
     # Other Configs
-
-    THUMB = config(
-        "THUMBNAIL", default="https://graph.org/file/37d9d0657d51e01a71f26.jpg"
-    )
-    FFMPEG = config("FFMPEG", default="ffmpeg")
+    THUMB = config("THUMBNAIL", default=os.path.join(os.path.dirname(__file__), "default_thumb.jpg"))
+    FFMPEG = config("FFMPEG", default="ffmpeg", exists_ok=True)
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=False, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
+
+    @staticmethod
+    def get_channel_ids():
+        return Var.BACKUP_CHANNEL, Var.MAIN_CHANNEL, Var.LOG_CHANNEL, Var.CLOUD_CHANNEL
