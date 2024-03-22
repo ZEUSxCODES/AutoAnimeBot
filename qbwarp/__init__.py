5c8b431d91193da3c93e8436617d9a84c5057db9
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-22 06:31:45 +0530

    improve

--- qbwarp/__init__.py
+++ qbwarp/__init__.py
@@ -1,9 +1,9 @@
+import asyncio
 from torrentp import TorrentDownloader
 
-from bot.func import run_async
-
-
-@run_async
 def download_magnet(link: str, path: str):
-    torrent_file = TorrentDownloader(link, path)
-    return torrent_file.start_download()
+    async def download():
+        torrent_file = TorrentDownloader(link, path)
+        await torrent_file.start_download()
+
+    asyncio.run(download())
