d5051859fa557df0fe9477eb5c05b011b0eed841
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-22 06:31:52 +0530

    improve

--- pkg.sh
+++ pkg.sh
@@ -1,2 +1,16 @@
-sudo apt install -y mediainfo ffmpeg wget gcc python3-pip python3
-pip3 install -U -r requirements.txt
+#!/bin/bash
+
+# Install system dependencies
+set -e # exit on error
+if [[ ! $(command -v mediainfo) ]]; then
+    sudo apt-get update
+    sudo apt-get install -y mediainfo ffmpeg
+fi
+
+# Install Python dependencies
+if [[ ! $(command -v pip3) || ! -x "$(command -v pip3)" ]]; then
+    echo "pip3 not found, installing pip3"
+    sudo apt-get install -y python3-pip
+fi
+
+pip3 install --upgrade -r requirements.txt
