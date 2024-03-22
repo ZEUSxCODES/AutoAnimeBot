c25dee0d391b180073e6d012e8fbfabfde6f379c
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-22 06:31:36 +0530

    improve

--- bot/dts.py
+++ bot/dts.py
@@ -1,44 +1,52 @@
-#    This file is part of the AutoAnime distribution.
-#    Copyright (c) 2023 Kaif_00z
+# This file is part of the AutoAnime distribution.
+# Copyright (c) 2023 Kaif_00z
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
 #
-#    This program is distributed in the hope that it will be useful, but
-#    WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#    General Public License for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
 #
-# License can be found in <
-# https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE > .
-
+# License can be found in <https://github.com/kaif-00z/AutoAnimeBot/blob/main/LICENSE> .
 
 import json
+import sys
 
 import aiohttp
+from aiohttp import ClientSession
 
-from . import POST_TRACKER, Var, bot, os, reporter, sys
-from .rename import get_english
+import asyncio
+import time
 
+import os
+import re
+
+import logging
+
+import requests
+from bs4 import BeautifulSoup
+
+import pytz
+from datetime import datetime
+
+import telebot
+from telebot import types
+
+import psycopg2
+from psycopg2 import sql
+
+import traceback
+
+import config
+from . import Var, bot, reporter, POST_TRACKER, get_english
 
 async def shu_msg():
     if Var.SEND_SCHEDULE:
         try:
-            async with aiohttp.ClientSession() as ses:
+            async with ClientSession() as ses:
                 res = await ses.get(
                     "https://subsplease.org/api/?f=schedule&h=true&tz=Asia/Kolkata"
-                )
-                _res = await res.text()
-            xx = json.loads(_res)
-            xxx = xx["schedule"]
-            text = "**📆 Anime AirTime Today** `[IST]`\n\n"
-            for i in xxx:
-                text += f'`[{i["time"]}]` -  [{(await get_english(i["title"]))}](https://subsplease.org/shows/{i["page"]})\n'
-            mssg = await bot.send_message(Var.MAIN_CHANNEL, text)
-            await mssg.pin(notify=True)
-        except Exception as err:
-            await reporter.report(str(err), error=True, log=True)
-    POST_TRACKER.clear()
-    if Var.RESTART_EVERDAY:
-        os.execl(sys.executable, sys.executable, "-m", "bot", "--samedb", "--no-notify")
+              
