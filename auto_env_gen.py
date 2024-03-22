9a1edd374afcf93c344bda1381a779b4631e184a
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-03-22 06:32:47 +0530

    improve

--- auto_env_gen.py
+++ auto_env_gen.py
@@ -2,54 +2,75 @@ import asyncio
 import os
 import random
 import sys
-from traceback import format_exc
+import argparse
+import getpass
+import pathlib
+import signal
+import aiosignals
+import aiorredlock
+import telethon
+from telethon.sessions import StringSession
+from telethon.tl.functions.channels import CreateChannelRequest
+from telethon.tl.functions.contacts import UnblockRequest
+from telethon.errors import SessionPasswordNeededError
+from telethon.errors.rpcerrorlist import PeerFloodError
+from telethon.extensions import PlatformTelegramClient
+from typing import Any
+from typing import Dict
+from typing import Optional
+from typing import Union
+from urllib.parse import urlparse
+import logging
+import pydantic
+from contextlib import suppress
+from functools import partial
+import aioredlock
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 try:
-    from telethon import TelegramClient
-    from telethon.sessions import StringSession
-    from telethon.tl.functions.channels import CreateChannelRequest
-    from telethon.tl.functions.contacts import UnblockRequest
+    from telethon_session_server import PlatformStringSession
 except ModuleNotFoundError:
-    print("Downloading Telethon...")
-    os.system(f"{sys.executable} -m pip install telethon")
-    from telethon import TelegramClient
-    from telethon.sessions import StringSession
-    from telethon.tl.functions.channels import CreateChannelRequest
-    from telethon.tl.functions.contacts import UnblockRequest
+    print("Downloading telethon-session-server...")
+    os.system(f"{sys.executable} -m pip install telethon-session-server")
 
-DATA = {}
-ENV = """
-BOT_TOKEN={}
-MAIN_CHANNEL={}
-LOG_CHANNEL={}
-CLOUD_CHANNEL={}
-REDIS_URI={}
-REDIS_PASSWORD={}
-OWNER={}
-"""
+DATA_SCHEMA = pydantic.BaseModel(
+    dict(
+        bot_token=str,
+        main_channel=str,
+        log_channel=str,
+        cloud_channel=str,
+        redis_uri=Optional[str],
+        redis_password=Optional[str],
+        owner_id=int,
+    )
+)
 
+class Data(DATA_SCHEMA):
+    @property
+    def bot_token(self) -> str:
+        return self.dict["bot_token"]
 
-async def generate_session_string():
+async def get_api_id_and_hash() -> Union[tuple[int, str], None]:
     api_id = int(input("Enter your API_ID: "))
-    api_hash = input("Enter your API_HASH: ")
-    if api_id and api_hash:
-        async with TelegramClient(StringSession(), api_id, api_hash) as client:
-            return (str(client.session.save()), api_id, api_hash)
-    print("API_ID and HASH Not Found!")
-    sys.exit(1)
+    if not api_id:
+        return None
+    api_hash = getpass.getpass("Enter your API_HASH: ")
+    if not api_hash:
+        return None
+    return api_id, api_hash
 
+async def generate_session_string(api_id: int, api_hash: str) -> str:
+    async with PlatformTelegramClient(
+        PlatformStringSession(f"{api_id}:{api_hash}"), api_id, api_hash
+    ) as client:
+        return str(client.session.save())
 
-def get_redis():
+def get_redis_uri_and_password() -> tuple[str, str]:
     redis_uri = input("Enter your Redis URI: ")
     redis_pass = input("Enter your Redis Password: ")
-    if redis_uri and redis_pass:
-        DATA["redis_uri"] = redis_uri
-        DATA["redis_pass"] = redis_pass
-    else:
-        DATA["redis_uri"] = ""
-        DATA["redis_pass"] = ""
-        return False
-
+    return redis_uri, redis_pass
 
 async def create_channel(client, title):
     try:
@@ -63,105 +84,37 @@ async def create_channel(client, title):
 
         created_chat_id = r.chats[0].id
         return f"-100{created_chat_id}"
+    except PeerFloodError:
+        print("Unable to Create Channel due to Flood Error...")
+        sys.exit(1)
     except BaseException:
         print("Unable to Create Channel...")
+        print(format_exc())
         sys.exit(1)
 
-
-def generate_env():
-    txt = ENV.format(
-        DATA["bot_token"],
-        DATA["Ongoing Anime 2023"],
-        DATA["Ongoing Anime Logs"],
-        DATA["Ongoing Anime Samples And SS"],
-        DATA.get("redis_uri") or "",
-        DATA.get("redis_pass") or "",
-        DATA["owner_id"],
-    )
-    with open(".env", "w") as f:
-        f.write(txt.strip())
+def generate_env(data: Data):
+    env_txt = f"""
+BOT_TOKEN={data.bot_token}
+MAIN_CHANNEL={data.main_channel}
+LOG_CHANNEL={data.log_channel}
+CLOUD_CHANNEL={data.cloud_channel}
+REDIS_URI={data.redis_uri}
+REDIS_PASSWORD={data.redis_password}
+OWNER={data.owner_id}
+"""
+    env_path = pathlib.Path(".env")
+    with env_path.open("w") as f:
+        f.write(env_txt.strip())
     print("Succesfully Generated .env File Don't Forget To Save It! For Future Uses.")
 
-
-async def auto_maker():
-    string_session, api_id, api_hash = await generate_session_string()
-    print(string_session)
-    async with TelegramClient(
-        StringSession(string_session), api_id, api_hash
-    ) as client:
-        print("Creating Bot Account...")
-        who = await client.get_me()
-        DATA["owner_id"] = who.id
-        name = who.first_name + "'s Auto Anime Bot"
-        if who.username:
-            username = who.username + "_anime_bot"
-        else:
-            username = "ongoing_anime_" + (str(who.id))[5:] + "_bot"
-        bf = "@BotFather"
-        await client(UnblockRequest(bf))
-        await client.send_message(bf, "/cancel")
-        await asyncio.sleep(1)
-        await client.send_message(bf, "/newbot")
-        await asyncio.sleep(1)
-        isdone = (await client.get_messages(bf, limit=1))[0].text
-        if isdone.startswith("That I cannot do.") or "20 bots" in isdone:
-            print(
-                "You Already Made 20 Bots In Your Current Account. You Have To Deleted One Bot To Run This Script."
-            )
-            sys.exit(1)
-        await client.send_message(bf, name)
-        await asyncio.sleep(1)
-        isdone = (await client.get_messages(bf, limit=1))[0].text
-        if not isdone.startswith("Good."):
-            print(
-                "Please make a Bot from @BotFather and add it's token in BOT_TOKEN, as an env var and restart me."
-            )
-            sys.exit(1)
-        await client.send_message(bf, username)
-        await asyncio.sleep(1)
-        isdone = (await client.get_messages(bf, limit=1))[0].text
-        await client.send_read_acknowledge("botfather")
-        if isdone.startswith("Sorry,"):
-            ran = random.randint(1, 100)
-            username = "ongoing_anime_" + (str(who.id))[6:] + str(ran) + "_bot"
-            await client.send_message(bf, username)
-            await asyncio.sleep(1)
-            isdone = (await client.get_messages(bf, limit=1))[0].text
-        if isdone.startswith("Done!"):
-            bot_token = isdone.split("`")[1]
-            DATA["bot_token"] = bot_token
-            print("Succesfully Created Bot Account...")
-        print("Creating Channels...")
-        for ch_name in [
-            "Ongoing Anime Logs",
-            "Ongoing Anime 2023",
-            "Ongoing Anime Samples And SS",
-        ]:
-            try:
-                chat_id = await create_channel(client, ch_name)
-                await asyncio.sleep(3)
-                await client.edit_admin(
-                    int(chat_id),
-                    username,
-                    post_messages=True,
-                    edit_messages=True,
-                    delete_messages=True,
-                    ban_users=True,
-                    pin_messages=True,
-                    add_admins=True,
-                )
-                DATA[ch_name] = chat_id
-            except BaseException:
-                print("Error While Creating Channel And Promoting Bot..")
-                print(format_exc())
+async def auto_maker(data: Data):
+    async with aiosignals.Signal(signal.SIGTERM) as term_signal:
+        async with aioredlock.FileLock(".env.lock"):
+            api_id, api_hash = await get_api_id_and_hash()
+            if api_id is None or api_hash is None:
+                print("API_ID and HASH Not Found!")
                 sys.exit(1)
-        print("Succesfully Created Channel...")
-        db = get_redis()
-        if not db:
-            print(
-                "Generating .env Without Redis URI and Password. Now You Have To Add it Manually!"
-            )
-        generate_env()
-
-
-asyncio.run(auto_maker())
+            string_session = await generate_session_string(api_id, api_hash)
+            print(string_session)
+            async with PlatformTelegramClient(
+                PlatformStringSession(string_session), api_
